1942164c23ca4979b2eb0955e7cd556f29c44a31
Andres Rivero <andres19rivero@gmail.com>
2024-08-04 17:56:22 -0400

    fix: move home & semester to components folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { SemesterComponent } from './semester/semester.component';
+import { HomeComponent } from './components/home/home.component';
+import { SemesterComponent } from './components/semester/semester.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent},
