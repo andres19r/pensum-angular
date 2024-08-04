1c8075b85f05209e9c27d3e1e6249940737e0d97
Andres Rivero <andres19rivero@gmail.com>
2024-08-04 17:47:54 -0400

    generate home & semester components

---
+++ src/app/semester/semester.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-semester',
+  standalone: true,
+  imports: [],
+  templateUrl: './semester.component.html',
+  styleUrl: './semester.component.scss'
+})
+export class SemesterComponent {
+
+}
