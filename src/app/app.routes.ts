4431df1d1f9534916dea5077186ee1f5ea4029a5
Andres Rivero <andres19rivero@gmail.com>
2024-08-04 17:48:06 -0400

    set up the initial routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SemesterComponent } from './semester/semester.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'semester/:number', component: SemesterComponent},
+  { path: '**', redirectTo: 'home', pathMatch: 'full'}
+];
