c3c990e256ec69381e91612d4cf2e7227813f9d7
Andres Rivero <andres19rivero@gmail.com>
2024-08-07 00:13:04 -0400

    feat(home): inject service and show data in template

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Pensum } from '../../interfaces/pensum.interface';
+import { PensumService } from '../../services/pensum.service';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private readonly _pensumService = inject(PensumService);
 
+  pensum: Pensum = {} as Pensum;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this._pensumService.getPensum().subscribe((pensum) => {
+      this.pensum = pensum;
+    });
+  }
 }
