3e4f0a94162592c49a7ec71b5f61e182050d0662
Andres Rivero <andres19rivero@gmail.com>
2024-08-06 23:50:21 -0400

    feat(service): implement initial methods

--- src/app/services/pensum.service.ts
+++ src/app/services/pensum.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Pensum } from '../interfaces/pensum.interface';
+import { data } from '../data/db';
+import { Semester } from '../interfaces/semester.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PensumService {
+  constructor() {}
 
-  constructor() { }
+  getPensum(): Observable<Pensum> {
+    return of(data);
+  }
+
+  getSemesterByNumber(num: number): Observable<null | Semester> {
+    const semester = data.semesters.find((sem) => sem.num === num);
+    if (!semester) return of(null);
+
+    return of(semester);
+  }
 }
