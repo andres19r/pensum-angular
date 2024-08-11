79e7ac8d42886abc1f2f950087f5f1690c1f9dbb
Andres Rivero <andres19rivero@gmail.com>
2024-08-10 22:27:33 -0400

    feat(top-bar): implement initial template

--- src/app/shared/top-bar/top-bar.component.ts
+++ src/app/shared/top-bar/top-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatIconModule],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss',
 })
