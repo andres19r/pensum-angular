0bddb6f1640196a60201a1a9d445a4af54b5ecd1
Andres Rivero <andres19rivero@gmail.com>
2024-08-11 23:12:26 -0400

    refactor: move semester template to its own component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,18 +2,17 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Pensum } from '../../interfaces/pensum.interface';
 import { PensumService } from '../../services/pensum.service';
 import { CommonModule } from '@angular/common';
-import { MatTableModule } from '@angular/material/table';
+import { SemesterComponent } from '../semester/semester.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, SemesterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
   private readonly _pensumService = inject(PensumService);
-  displayedColumns: string[] = ['code', 'name', 'score', 'state', 'prerequisite']
 
   pensum: Pensum = {} as Pensum;
 
--- src/app/components/semester/semester.component.ts
+++ src/app/components/semester/semester.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Semester } from '../../interfaces/semester.interface';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-semester',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './semester.component.html',
   styleUrl: './semester.component.scss'
 })
 export class SemesterComponent {
-
+  @Input() semester: Semester = {} as Semester;
+  displayedColumns: string[] = ['code', 'name', 'score', 'state', 'prerequisite']
 }
