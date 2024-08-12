637f1ccf8777ae4904f5286cbf5a3e762d61b936
Andres Rivero <andres19rivero@gmail.com>
2024-08-12 00:06:20 -0400

    refactor(service): change methods to retrieve data with http client

--- src/app/services/pensum.service.ts
+++ src/app/services/pensum.service.ts
@@ -3,21 +3,26 @@ import { Observable, of } from 'rxjs';
 import { Pensum } from '../interfaces/pensum.interface';
 import { data } from '../data/db';
 import { Semester } from '../interfaces/semester.interface';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PensumService {
-  constructor() {}
+  private url: string = environment.baseUrl;
+
+  constructor(private http: HttpClient) {}
 
   getPensum(): Observable<Pensum> {
-    return of(data);
+    return this.http.get<Pensum>(this.url + 'pensum');
   }
 
-  getSemesterByNumber(num: number): Observable<null | Semester> {
-    const semester = data.semesters.find((sem) => sem.num === num);
-    if (!semester) return of(null);
+  getAllSemesters(): Observable<Semester[]> {
+    return this.http.get<Semester[]>(this.url + 'semesters');
+  }
 
-    return of(semester);
+  getSemesterById(id: number): Observable<Semester> {
+    return this.http.get<Semester>(this.url + `semesters/${id}`);
   }
 }
