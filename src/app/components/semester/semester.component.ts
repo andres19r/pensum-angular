1020a8a9d61ef3b9dbe304dff9ef09a21ac40fc0
Eduardo Rivero <eduardo.rivero@unosquare.com>
2024-08-13 23:32:42 -0400

    feat(semester): implement test functionality to create a subject

--- src/app/components/semester/semester.component.ts
+++ src/app/components/semester/semester.component.ts
@@ -1,16 +1,37 @@
-import { Component, Input } from '@angular/core';
-import { Semester } from '../../interfaces/semester.interface';
+import { Component, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { PensumService } from '../../services/pensum.service';
+import { Semester, Subject } from '../../interfaces';
 
 @Component({
   selector: 'app-semester',
   standalone: true,
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule, MatButtonModule],
   templateUrl: './semester.component.html',
-  styleUrl: './semester.component.scss'
+  styleUrl: './semester.component.scss',
 })
 export class SemesterComponent {
+  private readonly _pensumService = inject(PensumService);
   @Input() semester: Semester = {} as Semester;
-  displayedColumns: string[] = ['code', 'name', 'score', 'state', 'prerequisite']
+  displayedColumns: string[] = [
+    'code',
+    'name',
+    'score',
+    'state',
+    'prerequisite',
+  ];
+
+  addSubject() {
+    const mockSubjectToCreate: Subject = {
+      id: 'CAL-115',
+      name: 'Calculo III',
+      state: 'Approved',
+      score: 68,
+      prerequisite: 'none',
+      semester: 3
+    };
+    this._pensumService.createSubject(mockSubjectToCreate).subscribe(console.log)
+  }
 }
