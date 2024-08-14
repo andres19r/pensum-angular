0ac036e3ca0ab6dea228d09aab7cb7d0055a9c1c
Eduardo Rivero <eduardo.rivero@unosquare.com>
2024-08-14 18:58:00 -0400

    feat(components): generate create-subject comp

---
+++ src/app/componets/create-subject/create-subject.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-subject',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-subject.component.html',
+  styleUrl: './create-subject.component.scss'
+})
+export class CreateSubjectComponent {
+
+}
