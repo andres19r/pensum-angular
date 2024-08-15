2571808f541375a62dacf50991d8b69966328631
Eduardo Rivero <eduardo.rivero@unosquare.com>
2024-08-14 21:43:03 -0400

    feat(create-subject): implement reactive form with material fields

--- src/app/componets/create-subject/create-subject.component.ts
+++ src/app/componets/create-subject/create-subject.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-create-subject',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+  ],
   templateUrl: './create-subject.component.html',
-  styleUrl: './create-subject.component.scss'
+  styleUrl: './create-subject.component.scss',
 })
 export class CreateSubjectComponent {
+  subjectForm = this.formBuilder.group({
+    code: ['', Validators.required],
+    name: ['', Validators.required],
+    state: ['', Validators.required],
+    score: [50],
+    prerequisite: [''],
+    semester: [1, Validators.required],
+  });
 
+  constructor(private readonly formBuilder: FormBuilder) {}
 }
